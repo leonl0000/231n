5a07190e2ab232f2d3b2187d5bbaed62d90dd93d
leonl0000 <leonl@stanford.edu>
2018-06-07 04:52:18 +0000

    oaue gpu aoeuaoeuaoeu

--- Models/basic_models.py
+++ Models/basic_models.py
@@ -50,3 +50,25 @@ class two_layer_basic(nn.Module):
         ab = torch.cat((a1, b1), 1)
         y_hat = self.final(ab)
         return y_hat
+
+class two_d_basic(nn.Module):
+    def __init__(self, a_layers=[8],
+                        b_layers=[8],
+                        ab_layers=[1]):
+        super().__init__()
+        self.conv_a1 = nn.Conv2d(1, 8, 3, padding=1)
+        self.conv_b1 = nn.Conv2d(1, 8, 3, padding=1)
+        self.final = nn.Conv2d(16, 1, 1)
+        nn.init.kaiming_normal_(self.conv_a1.weight)
+        nn.init.kaiming_normal_(self.conv_b1.weight)
+        nn.init.kaiming_normal_(self.final.weight)
+    
+    def forward(self, x):
+        x1, x2 = x
+        a0 = x1[:,None,:,:,:]
+        b0 = x2[:,None,:,:,:]
+        a1 = F.relu(self.conv_a1(a0))
+        b1 = F.relu(self.conv_b1(b0))
+        ab = torch.cat((a1, b1), 1)
+        y_hat = self.final(ab)
+        return y_hat
